ced208af32f10bf58b40e909164c976b7ed7f586
Wilmer Gulcochia <karlwgs1989@gmail.com>
2024-06-29 22:00:31 -0500

    desafio-1

---
+++ desafio-1.js
@@ -0,0 +1,16 @@
+alert("¡Bienvenida y bienvenido a nuestro sitio web!");
+
+//et nombre = "Luna";
+//let edad = 25;
+let numeroDeVentas = 50;
+let saldoDisponible = 1000;
+
+alert("¡Error! Completa todos los campos");
+
+let mensajeDeError = "¡Error! Completa todos los campos";
+let nombre = prompt("Ingrese su nombre de usuario:");
+let edad = prompt("Ingrese su edad:");
+
+if (edad >= 18) {
+    alert("¡Puedes obtener tu licencia de conducir");
+}
