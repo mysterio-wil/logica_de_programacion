f871f3e492866773aa768b27bd70e7722423e868
Wilmer Gulcochia <karlwgs1989@gmail.com>
2024-06-29 21:42:43 -0500

    condicion if

--- app.js
+++ app.js
@@ -1,2 +1,7 @@
-alert("Hola mundo")
+let numeroSecreto = 6;
 let numeroUsuario = prompt("Me indicas un número por favor:");
+console.log(numeroUsuario);
+
+if (numeroUsuario == numeroSecreto) {
+    alert("Has acertado!");
+}
