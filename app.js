e623da831d7e3e35c6aa51ea4abf64f6f9dcd574
Wilmer Gulcochia <karlwgs1989@gmail.com>
2024-06-29 22:40:32 -0500

    comentarios y else

--- app.js
+++ app.js
@@ -1,7 +1,12 @@
+// Variables
 let numeroSecreto = 6;
 let numeroUsuario = prompt("Me indicas un número por favor:");
 console.log(numeroUsuario);
-
+/* Este código realiza la comparación */
 if (numeroUsuario == numeroSecreto) {
+    // Acertamos, fue verdadera la condición.
     alert("Has acertado!");
+} else {
+    // No acertamos, fue falsa la condición.
+    alert("Lo siento, no acertaste el número");
 }
