75eb20358c63c19d94a88069a6f30eb1de6c1c9f
Wilmer Gulcochia <karlwgs1989@gmail.com>
2024-07-01 10:23:22 -0500

    blucle while

--- app.js
+++ app.js
@@ -1,17 +1,21 @@
 // Variables
 let numeroSecreto = 6;
-let numeroUsuario = prompt("Me indicas un número por favor:");
-console.log(numeroUsuario);
-/* Este código realiza la comparación */
-if (numeroUsuario == numeroSecreto) {
-    // Acertamos, fue verdadera la condición.
-    alert(`Acertaste, el número es: ${numeroUsuario}`);
-} else {
-    if (numeroUsuario > numeroSecreto){
-        alert(`El número secreto es menor`);
-    } else{
-        alert(`El número secreto es mayor`);
+let numeroUsuario = 0;
+while(numeroUsuario != numeroSecreto) {
+    numeroUsuario = prompt("Me indicas un número por favor:");
+    console.log(numeroUsuario);
+    /* Este código realiza la comparación */
+    if (numeroUsuario == numeroSecreto) {
+        // Acertamos, fue verdadera la condición.
+        alert(`Acertaste, el número es: ${numeroUsuario}`);
+    } else {
+        if (numeroUsuario > numeroSecreto){
+            alert(`El número secreto es menor`);
+        } else{
+            alert(`El número secreto es mayor`);
+        }
+        // No acertamos, fue falsa la condición.
+        // alert("Lo siento, no acertaste el número");
     }
-    // No acertamos, fue falsa la condición.
-    // alert("Lo siento, no acertaste el número");
 }
+    
