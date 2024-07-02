e49331600ed5c15098aaa345dc6c62e1cd06741d
Wilmer Gulcochia <karlwgs1989@gmail.com>
2024-07-02 12:21:47 -0500

    contador de intentos

--- app.js
+++ app.js
@@ -1,19 +1,24 @@
 // Variables
 let numeroSecreto = 6;
 let numeroUsuario = 0;
+let intentos = 1;
+let palabraVeces = "vez";
 while(numeroUsuario != numeroSecreto) {
     numeroUsuario = prompt("Me indicas un número por favor:");
     console.log(numeroUsuario);
     /* Este código realiza la comparación */
     if (numeroUsuario == numeroSecreto) {
         // Acertamos, fue verdadera la condición.
-        alert(`Acertaste, el número es: ${numeroUsuario}`);
+        alert(`Acertaste, el número es: ${numeroUsuario}. Lo hiciste en ${intentos} ${palabraVeces}`);
     } else {
         if (numeroUsuario > numeroSecreto){
             alert(`El número secreto es menor`);
         } else{
             alert(`El número secreto es mayor`);
         }
+        // Incrementamos el contador cuando no acierta
+        intentos++;
+        palabraVeces = "veces";
         // No acertamos, fue falsa la condición.
         // alert("Lo siento, no acertaste el número");
     }
