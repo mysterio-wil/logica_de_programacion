05884600093bc62c4ac1503b0f80060dd92fae03
Wilmer Gulcochia <karlwgs1989@gmail.com>
2024-07-02 13:45:03 -0500

    se actualizó el desafio-3

--- desafio-3.js
+++ desafio-3.js
@@ -2,12 +2,14 @@
 let contador = 1;
 while (contador <= 10) {
     console.log(contador);
-}
+    contador++;
+    }
 
 /*2. Crea un contador que comience en 10 y vaya hasta 0 usando un bucle 'while'. Muestra cada número.*/
 let contador2 = 10;
 while (contador2 >= 0) {
     console.log(contador2);
+    contador2--;
 }
 
 /*3. Crea un programa de cuenta progresiva. Pide un número y cuenta desde 0 hasta ese número utilizando un bucle 'while' en la consola del navegador. */
@@ -15,4 +17,14 @@ let numero = prompt("Introduce un número: ");
 let contador3 = 0;
 while (contador3 <= numero) {
     console.log(contador3);
+    contador3++;
+}
+
+/*Crea un programa de cuenta progresiva. Pide un número y cuenta desde 0 hasta ese número utilizando un bucle 'while' en la consola del navegador.
+ */
+let numero2 = prompt("Introduce un número: ");
+let contador4 = 0;
+while (contador4 <= numero2) {
+    console.log(contador4);
+    contador4++;
 }
