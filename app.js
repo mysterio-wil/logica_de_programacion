f720f168aa9a53ad592118be5cacb2d01a511ef2
Wilmer Gulcochia <karlwgs1989@gmail.com>
2024-07-03 16:07:51 -0500

    operador ternario

--- app.js
+++ app.js
@@ -2,14 +2,15 @@
 let numeroSecreto = 6;
 let numeroUsuario = 0;
 let intentos = 1;
-let palabraVeces = "vez";
+let maximoIntentos = 2;
+// let palabraVeces = "vez";
 while(numeroUsuario != numeroSecreto) {
     numeroUsuario = prompt("Me indicas un número por favor:");
     console.log(numeroUsuario);
     /* Este código realiza la comparación */
     if (numeroUsuario == numeroSecreto) {
         // Acertamos, fue verdadera la condición.
-        alert(`Acertaste, el número es: ${numeroUsuario}. Lo hiciste en ${intentos} ${palabraVeces}`);
+        alert(`Acertaste, el número es: ${numeroUsuario}. Lo hiciste en ${intentos} ${intentos == 1 ? "vez": "veces"}`);
     } else {
         if (numeroUsuario > numeroSecreto){
             alert(`El número secreto es menor`);
@@ -17,8 +18,15 @@ while(numeroUsuario != numeroSecreto) {
             alert(`El número secreto es mayor`);
         }
         // Incrementamos el contador cuando no acierta
+        // intentos = intentos + 1;
+        // intentos += 1;
         intentos++;
-        palabraVeces = "veces";
+
+        // palabraVeces = "veces";
+        if (intentos > maximoIntentos) {
+            alert(`Llegaste al número máximo de ${intentos} intentos`);
+            break
+        }
         // No acertamos, fue falsa la condición.
         // alert("Lo siento, no acertaste el número");
     }
